fd5821e0c9ec75e5f12b0d75dd381e7a3529599e
Hussein <41930483@students.liu.edu.lb>
2024-02-25 17:15:11 +0200

    Add BookModel class

---
+++ E_LibraryApi/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿namespace E_LibraryApi.Models
+{
+    public class BookModel
+    {
+        public Guid Id { get; set; }
+        public string BookName { get; set; }
+        public string  BookAuthor { get; set; }
+        public string BookPublication { get; set; }
+        public double BookPrice { get; set; }
+        public DateTime BookPurhcaseDate { get; set; }
+        public int BookQuantity { get; set; }
+
+
+    }
+}
