6915a3e9ad46c3421cc35dd4f39e9c5921137dce
Hussein <41930483@students.liu.edu.lb>
2024-02-25 17:15:23 +0200

    Add E_LibDb dependency and implement Cancel and Save functionality in AddBookForm

--- E_LibraryManagementSystem/AddBookForm.xaml.cs
+++ E_LibraryManagementSystem/AddBookForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_LibraryManagementSystem.Db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,40 @@ namespace E_LibraryManagementSystem
     /// </summary>
     public partial class AddBookForm : Window
     {
-        public AddBookForm()
+        private readonly E_LibDb db;
+
+        public AddBookForm(E_LibDb db)
         {
             InitializeComponent();
+            this.db = db;
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var book in BookSubForm.Children)
+            {
+                if(book is TextBox box)
+                {
+                    box.Clear();
+                }
+                if(book is DatePicker picker)
+                {
+                    picker.SelectedDate = DateTime.Now;
+                }
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Int64 price = Int64.Parse(Book_Price.Text);
+            Int64 quantity = Int64.Parse(BookQuantity.Text);
+
+            if(MessageBox.Show("Are you sure to save this book?","Save Book",MessageBoxButton.OKCancel,MessageBoxImage.Question) == MessageBoxResult.OK)
+            {
+                MessageBox.Show("Book Saved Successfully", "Save Book", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+            
         }
     }
 }
