2eff4de3799b9a415872630dd62a5d250db4881a
Hussein <41930483@students.liu.edu.lb>
2024-02-25 21:23:51 +0200

    Add migration for Book model

---
+++ E_LibraryApi/Migrations/20240225154102_addbookmodel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_LibraryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbookmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BookAuthor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BookPublication = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BookPrice = table.Column<double>(type: "float", nullable: false),
+                    BookPurhcaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BookQuantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
