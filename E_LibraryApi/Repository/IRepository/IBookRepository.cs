554373cc0eb5099969076be66b7a74a012f1845f
Hussein <41930483@students.liu.edu.lb>
2024-02-25 21:24:11 +0200

    Add IBookRepository interface

---
+++ E_LibraryApi/Repository/IRepository/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using E_LibraryApi.Models;
+using E_LibraryApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace E_LibraryApi.Repository.IRepository
+{
+    public interface IBookRepository
+    {
+        Task<List<BookModel>> GetAllBooks(Expression<Func<BookModel,bool>> filter = null);
+        Task<BookModel> GetBook(Expression<Func<BookModel,bool>> filter = null,bool tracked = true);
+        Task<bool> BookExists (string bookName);
+        Task CreateBook(BookModel book);
+        Task UpdateBook(BookModel book);
+        Task DeleteBook(BookModel bookId);
+        Task Save(); 
+    }
+}
