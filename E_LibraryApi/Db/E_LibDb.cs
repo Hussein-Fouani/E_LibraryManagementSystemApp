1ce55b522e24d4e4fbd8b05e41a93bb52d934002
Hussein <41930483@students.liu.edu.lb>
2024-02-25 11:25:06 +0200

    Add E_LibDb class to handle database operations

---
+++ E_LibraryApi/Db/E_LibDb.cs
@@ -0,0 +1,30 @@
+﻿using E_LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_LibraryManagementSystem.Db
+{
+    public class E_LibDb:DbContext
+    {
+        public E_LibDb(DbContextOptions<E_LibDb> options):base(options)
+        {
+            
+        }
+        public DbSet<SignInModel> SignIn { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SignInModel>().HasData(
+                new SignInModel
+                {
+                    Id =Guid.Parse("e6cb49e3-6905-4800-a6ca-a9b645ab18ef"),
+                    UserName = "Admin",
+                    Password = "Admin"
+                }
+                );
+        }
+    }
+}
