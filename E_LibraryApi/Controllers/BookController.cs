f7e1247cd5bf5663c4132aeabbbd41f05385dfe2
Hussein <41930483@students.liu.edu.lb>
2024-02-25 17:35:48 +0200

    Add BookController with AddBook endpoint

---
+++ E_LibraryApi/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using E_LibraryApi.Models;
+using E_LibraryManagementSystem.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly E_LibDb db;
+
+        
+        public BookController(E_LibDb db)
+        {
+            this.db = db;
+        }
+        [HttpPost]
+        public  async Task<IActionResult> AddBook([FromBody] BookModel book)
+        {
+            if(book == null)
+            {
+                return BadRequest("Couldn't Add Book"); }
+            
+            if (ModelState.IsValid)
+            {
+                await db.Book.AddAsync(book);
+                await db.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
