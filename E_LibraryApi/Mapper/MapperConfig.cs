bd3a995840b2cd197371c3707fc4630cdd9e3b0d
Hussein <41930483@students.liu.edu.lb>
2024-02-25 21:23:46 +0200

    Add MapperConfig class for AutoMapper configuration

---
+++ E_LibraryApi/Mapper/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using E_LibraryManagementSystem.Models;
+
+namespace E_LibraryApi.Mapper
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Models.BookModel, Models.Dto.BookDto>().ReverseMap();
+            CreateMap<SignInModel, Models.Dto.SignInDto>().ReverseMap();
+        }
+    }
+}
