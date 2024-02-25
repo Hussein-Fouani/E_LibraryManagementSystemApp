76c55e4fb4a5e08d442ebbdacbc02531d127b003
Hussein <41930483@students.liu.edu.lb>
2024-02-25 21:23:32 +0200

    Add AutoMapper, Mapper, and BookDto to BookController

--- E_LibraryApi/Controllers/BookController.cs
+++ E_LibraryApi/Controllers/BookController.cs
@@ -1,4 +1,8 @@
-﻿using E_LibraryApi.Models;
+﻿using AutoMapper;
+using E_LibraryApi.Mapper;
+using E_LibraryApi.Models;
+using E_LibraryApi.Models.Dto;
+using E_LibraryApi.Repository.IRepository;
 using E_LibraryManagementSystem.Db;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,26 +14,65 @@ namespace E_LibraryApi.Controllers
     public class BookController : ControllerBase
     {
         private readonly E_LibDb db;
+        private readonly IMapper mapper;
+        private readonly IBookRepository bookRepository;
 
-        
-        public BookController(E_LibDb db)
+        public BookController(E_LibDb db,IMapper mapper,IBookRepository bookRepository)
         {
             this.db = db;
+            this.mapper = mapper;
+            this.bookRepository = bookRepository;
         }
         [HttpPost]
-        public  async Task<IActionResult> AddBook([FromBody] BookModel book)
+        public  async Task<IActionResult> CreateBook([FromBody] BookDto book)
         {
             if(book == null)
             {
-                return BadRequest("Couldn't Add Book"); }
-            
+                return BadRequest("Couldn't Add Book"); 
+            }
+
+            if ( await bookRepository.BookExists(book.BookName))
+            {
+                ModelState.AddModelError("BookName", "Book Name already exists");
+                return BadRequest("Book Name already exists");
+            }
             if (ModelState.IsValid)
             {
-                await db.Book.AddAsync(book);
-                await db.SaveChangesAsync();
-                return Ok();
+                var books = mapper.Map<BookModel>(book);
+                await bookRepository.CreateBook(books);
+                return CreatedAtRoute("CreateBook", new {Id=Guid.NewGuid()},books);
             }
             return BadRequest();
         }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> DeleteBook(Guid id)
+        {
+            var book = await bookRepository.GetBook(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            await bookRepository.DeleteBook(book);
+            return NoContent();
+        }
+        [HttpPut("{Id:Guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateBook([FromBody]BookDto bookdto,Guid Id)
+        {
+            if (bookdto == null)
+            {
+                return BadRequest();
+            }
+            var book = await bookRepository.GetBook(b => b.Id == Id,tracked:false);
+            
+            var mapped =  mapper.Map<BookModel>(bookdto);
+
+            await bookRepository.UpdateBook(mapped);
+            return Ok("Updated SuccessFully");
+        }
+
     }
 }
