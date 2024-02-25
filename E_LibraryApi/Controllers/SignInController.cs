f19e1786c827bfac6946c8e3897d79c05324fa80
Hussein <41930483@students.liu.edu.lb>
2024-02-25 11:24:58 +0200

    Add SignInController to handle user sign-in

---
+++ E_LibraryApi/Controllers/SignInController.cs
@@ -0,0 +1,27 @@
+﻿using E_LibraryApi.Models.Dto;
+using E_LibraryManagementSystem.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignInController : ControllerBase
+    {
+        private readonly E_LibDb db;
+
+        public SignInController(E_LibDb db)
+        {
+            this.db = db;
+        }
+        [HttpGet("Id:Guid")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> SignInUser(SignInDto signindto)
+        {
+            return null;
+        }
+    }
+}
