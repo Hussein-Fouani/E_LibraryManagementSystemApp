80a0f6c58459da2c8ad61fded95b32c6d3756b6c
Hussein <41930483@students.liu.edu.lb>
2024-02-26 19:11:06 +0200

    Add Student model class

---
+++ E_LibraryApi/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace E_LibraryApi.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string Department { get; set; }
+        public int EnrollmentNb { get; set; }
+        public string StudentSemester { get; set; }
+        public int StudentContact { get; set; }
+
+    }
+}
