24075e7a63e0082127b24a31d99768284acb2684
Hussein <41930483@students.liu.edu.lb>
2024-02-26 19:11:14 +0200

    Add StudentController class

---
+++ E_LibraryApi/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMapper mapper;
+
+        public StudentController(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+    }
+}
