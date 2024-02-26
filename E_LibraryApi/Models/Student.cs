5c749da8fc65d412cde13387b8ef5a446ebf688d
Hussein <41930483@students.liu.edu.lb>
2024-02-26 19:11:17 +0200

    Add validation attributes to Student model

--- E_LibraryApi/Models/Student.cs
+++ E_LibraryApi/Models/Student.cs
@@ -1,13 +1,35 @@
-﻿namespace E_LibraryApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_LibraryApi.Models
 {
     public class Student
     {
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
         public string StudentName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(100)]
+        [MinLength(20)]
         public string StudentEmail { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
         public string Department { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [MinLength(5)]
         public int EnrollmentNb { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(5)]
         public string StudentSemester { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(15)]
+        [MinLength(10)]
+        [Required]
         public int StudentContact { get; set; }
 
     }
