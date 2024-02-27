4e24a013d395c3a2206c0f2ef60eb632ca9f6eaa
Hussein <41930483@students.liu.edu.lb>
2024-02-27 15:18:02 +0200

    Add CRUD methods to IStudentRepository interface

--- E_LibraryApi/Repository/IRepository/IStudentRepository.cs
+++ E_LibraryApi/Repository/IRepository/IStudentRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace E_LibraryApi.Repository.IRepository
+﻿using E_LibraryApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LibraryApi.Repository.IRepository
 {
     public interface IStudentRepository
     {
-        Task 
+        Task<List<StudentDto>> GetAllStudents();
+        Task<StudentDto> GetStudent(Guid studentId);
+        Task<bool> StudentExists(string studentName);
+        Task CreateStudent(StudentDto student);
+        Task UpdateStudent(StudentDto student);
+        Task DeleteStudent(Guid studentId);
+        Task Save();
     }
 }
