240ada2fd859c881c00f9f9fb8916361a88dcff3
Hussein <41930483@students.liu.edu.lb>
2024-02-28 18:17:05 +0200

    Add ISignUp interface to IRepository

---
+++ E_LibraryApi/Repository/IRepository/ISignUp.cs
@@ -0,0 +1,12 @@
+﻿using E_LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LibraryApi.Repository.IRepository
+{
+    public interface ISignUp
+    {
+        Task SignUpAsync(SignUpModel signUpModel);
+        Task Save();
+        Task<bool> CheckIfUserExists(string Username);
+    }
+}
