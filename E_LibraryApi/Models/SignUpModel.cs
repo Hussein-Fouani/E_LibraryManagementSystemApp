a9fe110602530ea1400ae9701b455a21eba7ffbe
Hussein <41930483@students.liu.edu.lb>
2024-02-28 18:16:56 +0200

    Add validation attributes to SignUpModel

--- E_LibraryApi/Models/SignUpModel.cs
+++ E_LibraryApi/Models/SignUpModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,17 @@ namespace E_LibraryManagementSystem.Models
 {
     public class SignUpModel
     {
-
+        [Key]
+        public Guid UserId { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [MinLength(4)]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
