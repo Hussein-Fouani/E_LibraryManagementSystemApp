a266f2091fd6240df6847cc9dcc83a70f90bda5e
Hussein <41930483@students.liu.edu.lb>
2024-02-28 22:45:21 +0200

    Add ISignInRepository interface

---
+++ E_LibraryApi/Repository/IRepository/ISignInRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_LibraryManagementSystem.Models;
+
+namespace E_LibraryApi.Repository.IRepository
+{
+    public interface ISignInRepository
+    {
+        
+        Task<bool> AuthenticateAsync(string userName, string password);
+        Task SignInAsync(SignInModel model);
+    }
+}
