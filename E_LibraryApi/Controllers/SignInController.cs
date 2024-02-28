78b236202e382e934248fbea0db0f55fa93bdfdb
Hussein <41930483@students.liu.edu.lb>
2024-02-29 00:04:25 +0200

    Add SignInRepository and implement SignInUser endpoint validation

--- E_LibraryApi/Controllers/SignInController.cs
+++ E_LibraryApi/Controllers/SignInController.cs
@@ -1,7 +1,9 @@
 ﻿using E_LibraryApi.Models.Dto;
+using E_LibraryApi.Repository;
 using E_LibraryManagementSystem.Db;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace E_LibraryApi.Controllers
 {
@@ -10,18 +12,32 @@ namespace E_LibraryApi.Controllers
     public class SignInController : ControllerBase
     {
         private readonly E_LibDb db;
+        private readonly SignInRepository signInRepository;
 
-        public SignInController(E_LibDb db)
+        public SignInController(E_LibDb db,SignInRepository signInRepository)
         {
             this.db = db;
+            this.signInRepository = signInRepository;
         }
-        [HttpGet("Id:Guid")]
+        [HttpGet("username:string")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> SignInUser(SignInDto signindto)
         {
-            return null;
+            if(signindto == null)
+            {
+                return BadRequest("Please Supply UserInfo");
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(string.IsNullOrEmpty(signindto.UserName) || string.IsNullOrEmpty(signindto.Password))
+            {
+                return BadRequest("Username and Password must be at least 4 characters");
+            }
+
         }
     }
 }
