c101e8f68168ff58878f9575831c68c0c31adaeb
Hussein <41930483@students.liu.edu.lb>
2024-02-29 00:04:31 +0200

    Add SignInController and logger to SignInRepository

--- E_LibraryApi/Repository/SignInRepository.cs
+++ E_LibraryApi/Repository/SignInRepository.cs
@@ -1,25 +1,31 @@
-﻿using E_LibraryApi.Repository.IRepository;
+﻿using E_LibraryApi.Controllers;
+using E_LibraryApi.Repository.IRepository;
 using E_LibraryManagementSystem.Db;
 using E_LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_LibraryApi.Repository
 {
     public class SignInRepository:ISignInRepository
     {
         private readonly E_LibDb db;
+        private readonly Logger<SignInController> logger;
 
-        public SignInRepository(E_LibDb db)
+        public SignInRepository(E_LibDb db,Logger<SignInController> logger)
         {
             this.db = db;
+            
+            this.logger = logger;
         }
         public Task<bool> AuthenticateAsync(string userName, string password)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task SignInAsync(SignInModel model)
+        public async Task<SignInModel>  SignInAsync(SignInModel model)
         {
-            
+          return  await db.SignIn.FirstOrDefaultAsync(m=>m.UserName==model.UserName);  
         }
     }
    
