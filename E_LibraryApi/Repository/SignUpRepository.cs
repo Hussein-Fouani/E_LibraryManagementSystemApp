88ebc6253489989e9715121e466764b4958b0724
Hussein <41930483@students.liu.edu.lb>
2024-02-28 18:17:00 +0200

    Add SignUpRepository class

---
+++ E_LibraryApi/Repository/SignUpRepository.cs
@@ -0,0 +1,37 @@
+﻿using E_LibraryApi.Repository.IRepository;
+using E_LibraryManagementSystem.Db;
+using E_LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_LibraryApi.Repository
+{
+    public class SignUpRepository : ISignUp
+    {
+        private readonly E_LibDb db;
+
+        public SignUpRepository(E_LibDb db)
+        {
+            this.db = db;
+        }
+
+        public async Task<bool> CheckIfUserExists(string username)
+        {
+           var user = await db.SignIn.FirstOrDefaultAsync(x => x.UserName == username);
+            
+            return user!=null;
+        }
+
+        public async Task Save()
+        {
+            await db.SaveChangesAsync();
+        }
+
+        public async Task SignUpAsync(SignUpModel signUpModel)
+        {
+            await db.SignUp.AddAsync(signUpModel);
+            await Save();
+
+        }
+    }
+}
