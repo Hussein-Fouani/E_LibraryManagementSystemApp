a458a27611ae90e842a8031e4dffdeebb37960dc
Hussein <41930483@students.liu.edu.lb>
2024-02-28 22:54:53 +0200

    Add SignInRepository constructor to inject E_LibDb

--- E_LibraryApi/Repository/SignInRepository.cs
+++ E_LibraryApi/Repository/SignInRepository.cs
@@ -1,10 +1,17 @@
 ﻿using E_LibraryApi.Repository.IRepository;
+using E_LibraryManagementSystem.Db;
 using E_LibraryManagementSystem.Models;
 
 namespace E_LibraryApi.Repository
 {
     public class SignInRepository:ISignInRepository
     {
+        private readonly E_LibDb db;
+
+        public SignInRepository(E_LibDb db)
+        {
+            this.db = db;
+        }
         public Task<bool> AuthenticateAsync(string userName, string password)
         {
             throw new System.NotImplementedException();
@@ -12,7 +19,7 @@ namespace E_LibraryApi.Repository
 
         public Task SignInAsync(SignInModel model)
         {
-            throw new System.NotImplementedException();
+            
         }
     }
    
