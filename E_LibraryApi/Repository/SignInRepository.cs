5ed0435bb0acda8419ddbd48945ea9bf9433c7df
Hussein <41930483@students.liu.edu.lb>
2024-02-28 22:45:13 +0200

    Add SignInRepository class

---
+++ E_LibraryApi/Repository/SignInRepository.cs
@@ -0,0 +1,19 @@
+﻿using E_LibraryApi.Repository.IRepository;
+using E_LibraryManagementSystem.Models;
+
+namespace E_LibraryApi.Repository
+{
+    public class SignInRepository:ISignInRepository
+    {
+        public Task<bool> AuthenticateAsync(string userName, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SignInAsync(SignInModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+   
+}
