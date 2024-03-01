9fd7693513522359e009921899a4f0e7a02d156a
Hussein <41930483@students.liu.edu.lb>
2024-03-01 15:56:00 +0200

    Update GetStudent method signature

--- E_LibraryApi/Repository/IRepository/IStudentRepository.cs
+++ E_LibraryApi/Repository/IRepository/IStudentRepository.cs
@@ -1,13 +1,14 @@
 ﻿using E_LibraryApi.Models;
 using E_LibraryApi.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace E_LibraryApi.Repository.IRepository
 {
     public interface IStudentRepository
     {
         Task<List<Student>> GetAllStudents();
-        Task<Student> GetStudent(Guid studentId);
+        Task<Student> GetStudent(Expression<Func<Student, bool>> filter);
         Task<bool> StudentExists(string studentName);
         Task CreateStudent(Student student);
         Task UpdateStudent(Student student);
