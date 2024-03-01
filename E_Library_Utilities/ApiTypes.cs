da4c8a9d39ac512fd0736f608401a78aa7791de6
Hussein <41930483@students.liu.edu.lb>
2024-03-01 18:03:32 +0200

    Add ApiTypes.cs file with ApiType enum

---
+++ E_Library_Utilities/ApiTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Library_Utilities
+{
+    public static class ApiTypes
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            DELETE,
+            PUT
+        } 
+    }
+}
