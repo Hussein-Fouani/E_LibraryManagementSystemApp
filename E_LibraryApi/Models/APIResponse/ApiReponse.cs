52001cd763f849955934056a781352edbba1f2ca
Hussein <41930483@students.liu.edu.lb>
2024-03-01 14:09:28 +0200

    Add APIResponse class

---
+++ E_LibraryApi/Models/APIResponse/ApiReponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace E_LibraryApi.Models.APIResponse
+{
+    public class ApiReponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
