be4391919f92d8183c288c5af76fe0b944f30fe8
Hussein <41930483@students.liu.edu.lb>
2024-03-02 10:53:02 +0200

    Add mapping configuration for models and DTOs

---
+++ E_LibraryManagementSystem/Mapper/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using E_LibraryManagementSystem.Models;
+using E_LibraryManagementSystem.Models.Dto;
+
+
+namespace E_LibraryManagementSystem.Mapper
+{
+    public class MappingConfig:Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<BookModel, BookDto>().ReverseMap();
+            CreateMap<SignInModel,SignInDto>().ReverseMap();
+            CreateMap<Student, StudentDto>().ReverseMap();
+        }
+    }
+}
