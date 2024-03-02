bf22b4ee1f5ad4fd837f3e8cfca7694d12547129
Hussein <41930483@students.liu.edu.lb>
2024-03-02 13:29:36 +0200

    Add IMapper dependency to SignInController

--- E_LibraryApi/Controllers/SignInController.cs
+++ E_LibraryApi/Controllers/SignInController.cs
@@ -1,6 +1,8 @@
-﻿using E_LibraryApi.Models.Dto;
+﻿using AutoMapper;
+using E_LibraryApi.Models.Dto;
 using E_LibraryApi.Repository;
 using E_LibraryManagementSystem.Db;
+using E_LibraryManagementSystem.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -13,11 +15,13 @@ namespace E_LibraryApi.Controllers
     {
         private readonly E_LibDb db;
         private readonly SignInRepository signInRepository;
+        private readonly IMapper mapper;
 
-        public SignInController(E_LibDb db,SignInRepository signInRepository)
+        public SignInController(E_LibDb db,SignInRepository signInRepository,IMapper mapper)
         {
             this.db = db;
             this.signInRepository = signInRepository;
+            this.mapper = mapper;
         }
         [HttpGet("username:string")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -25,19 +29,21 @@ namespace E_LibraryApi.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> SignInUser(SignInDto signindto)
         {
-            if(signindto == null)
+            if (signindto == null)
             {
                 return BadRequest("Please Supply UserInfo");
             }
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            if(string.IsNullOrEmpty(signindto.UserName) || string.IsNullOrEmpty(signindto.Password))
+            if (string.IsNullOrEmpty(signindto.UserName) || string.IsNullOrEmpty(signindto.Password))
             {
                 return BadRequest("Username and Password must be at least 4 characters");
             }
+            await signInRepository.SignInAsync(mapper.Map<SignInModel>(signindto));
+            return Ok("User Signed In");
 
         }
-    }
+    } 
 }
