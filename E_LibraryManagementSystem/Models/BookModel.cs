a76d860b880acfe14ca65faec44390915c36c8b3
Hussein <41930483@students.liu.edu.lb>
2024-03-02 15:44:57 +0200

    Refactor BookModel properties

--- E_LibraryManagementSystem/Models/BookModel.cs
+++ E_LibraryManagementSystem/Models/BookModel.cs
@@ -5,23 +5,17 @@ namespace E_LibraryManagementSystem.Models
     public class BookModel
     {
         public Guid Id { get; set; }
-        [Required]
-        [StringLength(100)]
+        
         public string BookName { get; set; }
-        [Required]
-        [StringLength(100)]
+      
         public string  BookAuthor { get; set; }
-        [Required]
-        [StringLength(100)]
+       
         public string BookPublication { get; set; }
-        [Required]
-        [DataType(DataType.Currency)]
+        
         public double BookPrice { get; set; }
-        [Required]
-        [DataType(DataType.Date)]
+       
         public DateTime BookPurhcaseDate { get; set; }
-        [Required]
-        [Range(1,5)]
+        
         public int BookQuantity { get; set; }
 
 
