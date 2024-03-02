99bdd2f353655b4537a8fe7787cd8adcf73ba0a1
Hussein <41930483@students.liu.edu.lb>
2024-03-02 13:29:42 +0200

    Add tables for Signup, Student, and IssuedStudentBooks

---
+++ E_LibraryApi/Migrations/20240302104055_addtables.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_LibraryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Signup",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Signup", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    StudentEmail = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EnrollmentNb = table.Column<int>(type: "int", maxLength: 10, nullable: false),
+                    StudentSemester = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    StudentContact = table.Column<int>(type: "int", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IssuedStudentBooks",
+                columns: table => new
+                {
+                    IssuedBookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssuedStudentBooks", x => x.IssuedBookId);
+                    table.ForeignKey(
+                        name: "FK_IssuedStudentBooks_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IssuedStudentBooks_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuedStudentBooks_BookId",
+                table: "IssuedStudentBooks",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuedStudentBooks_StudentId",
+                table: "IssuedStudentBooks",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IssuedStudentBooks");
+
+            migrationBuilder.DropTable(
+                name: "Signup");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
