160eaa9cad54b1b7ba9e378e8e5a292e9ee451da
Hussein <41930483@students.liu.edu.lb>
2024-03-08 14:26:47 +0200

    Add user authentication and authorization

--- E_LibraryApi/Program.cs
+++ E_LibraryApi/Program.cs
@@ -2,8 +2,14 @@ using E_LibraryApi.Mapper;
 using E_LibraryApi.Repository;
 using E_LibraryApi.Repository.IRepository;
 using E_LibraryManagementSystem.Db;
+using ELibrary.Domain.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +21,63 @@ builder.Services.AddControllers();
 builder.Services.AddLogging();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserRepository,UserRepository>();
 builder.Services.AddAutoMapper(typeof(MapperConfig));
+
 builder.Services.AddScoped<IBookRepository,BookRepository>();
 builder.Services.AddScoped<IStudentRepository,StudentRepository>();
+builder.Services.AddScoped<ISignUpRepository,SignUpRepository>();
 builder.Services.AddScoped<ISignInRepository,SignInRepository>();
-builder.Services.AddScoped<ISignUpRepository, SignUpRepository>();
+builder.Services.AddScoped<IBorrowBook,BorrowBook>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<E_LibDb>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+/*builder.Services.AddAuthentication(x =>
+{
+    x.DefaultScheme=JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata=false;
+    x.SaveToken=true;
+    x.TokenValidationParameters=new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey=true,
+        IssuerSigningKey=new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("ApiSettings:Secret").Value)),
+        ValidateIssuer=false,
+        ValidateAudience=false
+    };
+});*/
+
+/*builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.\r\r\n\n"
+        + "Enter 'Bearer' [space] and then your token in the text input below.\r\n\n"
+        + "Example: \"Bearer 12345abcdef\"\r\n\n",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme= "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,8 +88,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
